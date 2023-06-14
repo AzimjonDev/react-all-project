54b010c611840cdb15ee38c8f01aa6c0ade0cc95
azimjon <azimjonqoshoqov@gmail.com>
2023-06-14 14:03:32 +0500

    project

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Button from "./companents/button/button";
+import Input from "./companents/input/input";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+  <div>
+   <Button/>
+   <Input/>
+  </div>
+);
